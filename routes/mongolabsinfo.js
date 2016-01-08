08a195f2618aaa2facf15d1177baa9d1b73c16c5
Michael_Montero <mmontero2012@gmail.com>
2016-01-08 13:21:12 -0700

    changes

--- routes/mongolabsinfo.js
+++ routes/mongolabsinfo.js
@@ -1,4 +1,4 @@
 var mongoLabs=
-'mongodb://BIGTest:Pa55word!?@ds035613.mongolab.com:35613/babedata';
+'mongodb://gingermamba32:leigh1@ds039095.mongolab.com:39095/db_v2';
 
 exports.name = mongoLabs;
